098adba0f0bc32b7285f53e8df0d79267dfcd5e7
sadra1418 <sadra.vaziri@icloud.com>
2026-08-07 16:59:11 +0330

    Update my_api_lib.py

--- sever_with_myproject/apps/my_api_lib.py
+++ sever_with_myproject/apps/my_api_lib.py
@@ -76,7 +76,7 @@ class Api():
             
 
     async def giv_text(self ,time_out , dafee ):
-        await self.tab.wait_for_timeout(1000)
+        await self.tab.wait_for_timeout(6000)
         await self.tab.wait_for_selector(f'[data-virtual-list-item-key="{(dafee+1)*2}"]' , timeout=time_out)
         return await self.tab.inner_text(f'[data-virtual-list-item-key="{(dafee+1)*2}"]')
     
