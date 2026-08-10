d69e13dba51abd78cc66d4bbe7b9a08799c64174
sadra1418 <sadra.vaziri@icloud.com>
2026-08-10 15:21:46 +0330

    Update my_api_lib.py

--- sever_with_myproject/apps/my_api_lib.py
+++ sever_with_myproject/apps/my_api_lib.py
@@ -41,6 +41,7 @@ class Api():
 # ai api codes
     async def open_ai_website(self ,url='http://chat.deepseek.com/'):
         await self.tab.goto(url=url, wait_until='commit')
+        print(await self.tab.inner_html())
 
 
 
