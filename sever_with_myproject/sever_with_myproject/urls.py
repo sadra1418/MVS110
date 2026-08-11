285aeedaa9d5c835529c96cc5a33b98fb7993971
sadra1418 <sadra.vaziri@icloud.com>
2026-08-11 09:12:46 +0330

    Add URL for Google Search Console verification file

--- sever_with_myproject/sever_with_myproject/urls.py
+++ sever_with_myproject/sever_with_myproject/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from api.views import api , test , main_app
+from api.views import api , test , main_app , google_verification
 
 
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/' , api ),
     path( 'test/' , test),
     path('' , main_app),
+    path('googleccc612f328fb14c8.html', google_verification),
     
 ]
 
